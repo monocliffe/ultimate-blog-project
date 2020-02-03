cc1b42aabf7f99882381079a75655762aa0918ff
Stephen <stephen.cliffe@wpengine.com>
2020-02-03 10:59:48 +0000

    Redirects user to login to view protected page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,24 @@
 class ApplicationController < ActionController::Base
-
-  before_action :current_user
+  protect_from_forgery with: :exception
+  helper_method :current_user
+  before_action :ensure_login, except: [:index, :new]
 
   protected
 
   def current_user
     return unless session[:user_id]
     @current_user = User.where(id: session[:user_id]).first
+  end
+
+  def logged_in?
+    !@current_user.nil?
+  end
+
+  helper_method :logged_in?
+
+  def ensure_login
+    return true if logged_in?
+    session[:return_to] = request.fullpath
+    redirect_to login_path and return false
   end
 end
