f517fc4e3e87206a0bf1a8691683bb28f34c33e6
Stephen <stephen.cliffe@wpengine.com>
2020-02-04 17:08:17 +0000

    Modified Application Controller to resolve changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_user
-  #before_action :ensure_login, except: [:index, :new]
 
   protected
 
@@ -11,13 +10,13 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    !@current_user.nil?
+    @current_user.present?
   end
   helper_method :logged_in?
 
   def ensure_login
     return true if logged_in?
     session[:return_to] = request.fullpath
-    redirect_to login_path and return false
+    redirect_to login_path
   end
 end
