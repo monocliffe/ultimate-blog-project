3eacbbacaa84ce7fbd180a246b48ee401d2431c5
Stephen <stephen.cliffe@wpengine.com>
2020-02-12 16:46:57 +0000

    Added user association to posts & added ensure login to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :load_post, only: [:show]#, :edit, :destroy, :update]
+  before_action :ensure_login, except: [:index]
+
   def index
     @posts = Post.all
   end
@@ -9,17 +12,28 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = @current_user
     if @post.save
-      redirect_to post_path(@post), alert: 'Post created successfully !!'
+      redirect_to post_path(@post), alert: 'Post created successfully!'
     else
       render :new
     end
   end
+
+  def show; end
 
   private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def load_post
+    @post = Post.find_by(id: params[:id])
+
+    unless @post
+      redirect_to posts_path, alert: 'Post Not found!'
+    end
   end
 end
