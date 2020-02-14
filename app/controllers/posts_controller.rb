3ffe680d658839ff2a605103a74aa5983385d0f2
Stephen <stephen.cliffe@wpengine.com>
2020-02-14 14:32:01 +0000

    Added post list view, sorted by date.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :ensure_login, except: [:index]
 
   def index
-    @posts = Post.all
+    @posts = Post.order(:created_at)
   end
 
   def new
