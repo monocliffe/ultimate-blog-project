371ee8689105d46b8794d0f01935eb3d2da3b903
Stephen <stephen.cliffe@wpengine.com>
2020-03-06 11:22:49 +0000

    Added Post Controller tests for Update & Restore

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  context 'GET index' do
+  before :each do
+    @post = FactoryBot.create(:post)
+    session[:user_id] = @post.user.id
+  end
+
+  context 'index' do
     it 'returns a successful response' do
       get :index
       expect(response).to be_successful
     end
 
     it 'assigns @posts' do
-      post = FactoryBot.create(:post)
       get :index
-      expect(assigns(:posts)).to eq([post])
+      expect(assigns(:posts)).to eq([@post])
     end
 
     it 'renders index template' do
@@ -19,20 +23,14 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  context 'GET show' do
+  context 'show' do
     it 'renders the #show view' do
-      post = FactoryBot.create(:post)
-      get :show, params: { id: post.id }
+      get :show, params: { id: @post.id }
       expect(response).to render_template :show
     end
   end
 
   context 'destroy' do
-    before :each do
-      @post = FactoryBot.create(:post)
-      session[:user_id] = @post.user.id
-    end
-
     it 'should redirect to to index after deletion' do
       get :destroy, params: { id: @post.id }
       expect(response).to redirect_to(posts_url)
@@ -48,6 +46,33 @@ RSpec.describe PostsController, type: :controller do
       get :destroy, params: { id: @post.id }
       expect(response).to redirect_to(posts_url)
       expect(flash[:alert]).to match(/Undo/)
+    end
+
+    it 'should restore posts' do
+      get :destroy, params: { id: @post.id }
+      expect{
+        get :restore, params: { id: @post.id }
+      }.to change { Post.kept.count }.by(1)
+    end
+  end
+
+  context 'update' do
+    before :each do
+      get :update, params: { :id => @post.id, :post => { :title => 'test', :body => 'new body' } }
+    end
+
+    it 'should update content' do
+      @post.reload
+      expect(@post.title).to eq('test')
+      expect(@post.body.to_s).to include('new body')
+    end
+
+    it 'should redirect to posts_url' do
+      expect(response).to redirect_to(posts_url)
+    end
+
+    it 'should display success flash banner' do
+      expect(flash[:success]).to match(/Post updated successfully!/)
     end
   end
 end
